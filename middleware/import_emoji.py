2b243bebbc84cd864003c8d528f820958abffbf4
AnuR1234 <35266536+AnuR1234@users.noreply.github.com>
2022-12-15 20:28:05 +0100

    import file to load the emojis database into ES
    This import file is used to load the emojis database into Elasticsearch.
    
    Signed-off-by: AnuR1234 <35266536+AnuR1234@users.noreply.github.com>

---
+++ middleware/import_emoji.py
@@ -0,0 +1,45 @@
+import json   
+from Connection import client
+from elasticsearch import Elasticsearch, helpers
+import os     
+index_name = "emo31"
+with open("/Users/anureddy/Desktop/Sem01/DataScience_for_text_analytics/Project1/emojilib-main/dist/emoji-en-US.json") as fi:
+    
+    #reader = json.DictReader(fi, fieldnames=columns, delimiter=",", quotechar='"')
+
+    reader = json.loads(fi.read().encode('UTF-16'))
+
+#     # This skips the first row which is the header of the CSV file.
+#     # next(reader)
+    
+    actions = []
+#         # print(json.dumps(reader, indent=4))
+    for index in range(len(reader)):
+        action = {"index": {"_index": index_name}}
+        doc = {
+                 "id": int(index),
+                 "emoji": tuple(reader.items())[index][0],
+                 "meaning": tuple(reader.items())[index][1]
+             }
+        actions.append(json.dumps(action))
+        actions.append(json.dumps(doc))
+
+
+
+#     #with open("/Users/anureddy/Desktop/Sem01/DataScience_for_text_analytics/Project1/slang/slang.csv", "w") as fo:
+#         #fo.write("\n".join(actions))
+
+try:
+    # make the bulk call, and get a response
+    response = helpers.bulk(client, actions,index =index_name,doc_type = 'emojis')
+
+    #response = helpers.bulk(elastic, actions, index='employees', doc_type='people')
+    print ("\nRESPONSE:", response)
+except Exception as e:
+    print("\nERROR:", e)
+ 
+  
+index_name = "emo31"
+request = '''{"query": {"match_all": {}}}'''
+results = client.search(index=index_name, body=request,size = 100)['hits']['hits']
+print(results)
