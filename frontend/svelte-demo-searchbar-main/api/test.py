ffc460e95e8043139631de9789fdd5e48b38c4f4
AnuR1234 <35266536+AnuR1234@users.noreply.github.com>
2023-03-03 15:38:27 +0100

    Delete test.py
    Signed-off-by: AnuR1234 <35266536+AnuR1234@users.noreply.github.com>

--- frontend/svelte-demo-searchbar-main/api/test.py
+++
@@ -1,4 +0,0 @@
-with open('/Users/anureddy/Desktop/slang-translator/frontend/svelte-demo-searchbar-main/api/emoji_set.txt', 'r') as file:
-    # read the contents of the file
-    res = file.readlines()
-print(type(res))
